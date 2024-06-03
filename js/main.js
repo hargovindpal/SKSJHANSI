3b84f224fc1deb3d58af4a4971d42540043e35eb
harrypal <hargovindpal4@gmail.com>
2024-06-03 13:22:47 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -25,3 +25,26 @@ $(document).ready(function(){
     $("#myCarousel").carousel("next");
   });
 });
+
+//scroll to top functionality
+
+var scrollBtn = document.getElementById('scrollbtn');
+
+if (scrollBtn) {
+    // Show or hide the button based on scroll position
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollBtn.style.display = "block";
+        } else {
+            scrollBtn.style.display = "none";
+        }
+    };
+}
+
+// Function to scroll to the top
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+};
